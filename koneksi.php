ab9170e1359fd2c78a5bb9136e17316d7312ec99
fthrhys <fadhilfathirahesya@gmail.com>
2025-11-14 12:13:59 +0000

    Deploykhusus

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,15 @@
 <?php
-$host = "localhost";   
-$user = "root";        
-$pass = "";            
-$db   = "swarajatim";     
+$host = "db"; // nama service database di docker-compose
+$user = "swarajatim";
+$pass = "swarajatim123";
+$db   = "swarajatim_db";
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
+
+// Fix karakter aneh (�)
+mysqli_set_charset($koneksi, "utf8mb4");
 ?>
